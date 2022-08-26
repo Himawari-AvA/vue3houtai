52c1e96abc0d4f00f91ca01b5c5f920a0d5343ee
Himawari <867415074@qq.com>
2022-08-26 16:49:49 +0800

    feat: testGitcz

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
+/*
+ * @Author: Himawari 867415074@qq.com
+ * @Date: 2022-08-26 16:28:19
+ * @LastEditors: Himawari 867415074@qq.com
+ * @LastEditTime: 2022-08-26 16:37:30
+ * @FilePath: \vue_3.2\.eslintrc.js
+ * @Description:
+ *
+ * Copyright (c) 2022 by Himawari 867415074@qq.com, All Rights Reserved.
+ */
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: 0,
+    'space-before-function-paren': 0
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
+<!--
+ * @Author: Himawari 867415074@qq.com
+ * @Date: 2022-08-26 16:28:19
+ * @LastEditors: Himawari 867415074@qq.com
+ * @LastEditTime: 2022-08-26 16:37:51
+ * @FilePath: \vue_3.2\src\views\HomeView.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by Himawari 867415074@qq.com, All Rights Reserved. 
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
